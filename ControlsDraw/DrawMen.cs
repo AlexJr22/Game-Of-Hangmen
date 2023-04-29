06498d34351dfc4f0f866a6db498d579b6f66d17
Alex Junior <alexjr2oficial@gmail.com>
2023-04-29 16:32:54 -0300

    changing name folder

---
+++ ControlsDraw/DrawMen.cs
@@ -0,0 +1,26 @@
+namespace Game_Of_Hangmen.Controls;
+
+public static class DrawMan
+{
+    // this is the stickman drawing
+    //     O      -> DrawHead();
+    //  >--|--<   -> DrawArms();
+    //     |      -> DrawHalf();
+    //   _/ \_    -> DrawLegs();
+
+    public static void Drawing()
+    {
+        DrawHead();
+        DrawArms();
+        DrawHalf();
+        DrawLegs();
+    }
+
+    public static void DrawHead() => Console.WriteLine("   O   ");
+
+    public static void DrawArms() => Console.WriteLine(">--|--<");
+
+    public static void DrawHalf() => Console.WriteLine("   |   ");
+
+    public static void DrawLegs() => Console.WriteLine(" _/ \\_ ");
+}
