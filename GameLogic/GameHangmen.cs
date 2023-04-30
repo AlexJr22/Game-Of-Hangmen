1e0a0416e25ccb250dd68130babd88e4d0c14678
Alex Junior <alexjr2oficial@gmail.com>
2023-04-30 17:15:14 -0300

    setting the number of player attempts

--- GameLogic/GameHangmen.cs
+++ GameLogic/GameHangmen.cs
@@ -13,6 +13,7 @@ public class GameHangmen
     public GameHangmen()
     {
         this.Word = "";
+        HealthPoints = 4;
         this.Characters = new char[0];
         this.HiddenLetters = new char[0];
     }
